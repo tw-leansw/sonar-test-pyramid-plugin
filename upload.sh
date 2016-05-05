beb47d1dbdf85a0eb7179f43a805efa65da51c65
YongliuLi <ylli@thoughtworks.com>
2016-05-05 18:40:58 +0800

    [#0] delete the ip address

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 source "$HOME/.jenv/bin/jenv-init.sh"
 jenv use java 1.7.0_75
 mvn clean package
-scp ./target/sonar-lean-test-pyramid-plugin-1.0.0-SNAPSHOT.jar root@120.27.104.224:/root/
+scp ./target/sonar-lean-test-pyramid-plugin-1.0.0-SNAPSHOT.jar root@sonar-server::/root/
